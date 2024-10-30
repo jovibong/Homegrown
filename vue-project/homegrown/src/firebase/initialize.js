ee30dd62aeacfa35d0637f6a689be12b2b3e17cb
jonathanplwqg <jonathanlee.2023@smu.edu.sg>
2024-10-30 15:07:47 +0800

    FIREBASE FOLDER AND DEPENDENCIES ADDED
    includes firebase changes in the dependencies

---
+++ vue-project/homegrown/src/firebase/initialize.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+import { getFirestore } from "firebase/firestore";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDoIazOnYVJEuRJ9ANjRXeYPoVYX7jbHk4",
+  authDomain: "homegrown-52ddc.firebaseapp.com",
+  projectId: "homegrown-52ddc",
+  storageBucket: "homegrown-52ddc.appspot.com",
+  messagingSenderId: "237220468485",
+  appId: "1:237220468485:web:7c51d659c78e029a61096c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// init firestore service
+const db = getFirestore()
+export default db
