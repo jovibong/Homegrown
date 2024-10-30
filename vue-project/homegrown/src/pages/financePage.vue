c03e37504d317a27643beedf12e97aee2e4ff3fc
jovibong <jovi.bong.2023@scis.smu.edu.sg>
2024-10-30 15:04:38 +0800

    Revert "edited index.html"
    This reverts commit 0e9fd85deeafa1b9bb84dd6c84b823c0dba1aced.

---
+++ vue-project/homegrown/src/pages/financePage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="finance">
+        <h2>hello</h2>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+    props: {
+        msg: String
+    }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
