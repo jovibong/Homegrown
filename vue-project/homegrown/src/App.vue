1ad90482fbc926099e981a1a553e403803cff6a9
ShahulHameedBZR <shahul200627@gmail.com>
2024-10-30 15:19:53 +0800

    added footer

--- vue-project/homegrown/src/App.vue
+++ vue-project/homegrown/src/App.vue
@@ -2,15 +2,20 @@
   <div id="app">
       <nav-menu /> <!-- Include the header component -->
       <router-view /> <!-- This is where the current route's component will be rendered -->
+      <footer-bar /> 
+
   </div>
 </template>
 
 <script>
 import NavMenu from './components/NavMenu.vue'
+import FooterBar from './components/FooterBar.vue'
+
 
 export default {
   components: {
-    'nav-menu':NavMenu
+    'nav-menu':NavMenu,
+    'footer-bar':FooterBar
   }
 }
 </script>
